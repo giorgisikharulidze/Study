a2244bf873b9dc5e7c8636cd10234c311c407347
Adjarabet\g.sikharulidze <g.sikharulidze@adjarabet.com>
2023-11-19 20:36:34 +0400

    add next button, and his full functionalu

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //https://codebeautify.org/excel-to-json
+let wordCountsInPage = 52;
 
 let currentIndex = 0; // Initialize currentIndex
 let wordList; // Declare wordList at the top level
+// Add a variable to keep track of the current set of words
+let currentSetIndex = 0;
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const fileInput = document.getElementById('fileInput');
     const shuffleButton = document.getElementById('shuffleButton');
@@ -33,15 +38,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const checkbox = document.querySelector('.checkbox');
     checkbox.addEventListener('change', displayWords);
-
+//    const nextButton = document.getElementById('nextButton');
+//    nextButton.addEventListener('click', showNextWords);
 });
 
 
 function shuffleWords() {
     currentIndex = 0;
-    displayWords();
+    currentSetIndex = 0;
+    displayWords(wordList.slice(0, wordCountsInPage));
 }
-function displayWords() {
+function displayWords(wordList) {
     const wordListDiv = document.getElementById('wordList');
     const useForeignLanguage = document.querySelector('.checkbox').checked;
     const flipButton = document.getElementById('flipButton');
@@ -53,12 +60,13 @@ function displayWords() {
         const shuffledList = shuffleArray(wordList);
 
         // Take the first 12 elements or fewer if the array is smaller
-        const wordsToDisplay = shuffledList.slice(0, Math.min(52, shuffledList.length));
+        const wordsToDisplay = shuffledList.slice(0, Math.min(wordCountsInPage, shuffledList.length));
 
         const wordListDiv = document.getElementById('wordList');
 
         const wordsPerRow = 4; // Change to 4 columns
         const wordWidth = 100 / wordsPerRow; // Calculate the width of each word
+
         for (let i = 0; i < wordsToDisplay.length; i++) {
             // Create a new row for every 'wordsPerRow' words
             if (i % wordsPerRow === 0) {
@@ -126,17 +134,20 @@ function handleFile(event) {
                     const sheetName = workbook.SheetNames[0];
                     const sheet = workbook.Sheets[sheetName];
                     const jsonData = XLSX.utils.sheet_to_json(sheet);
-                    wordList = jsonData;
+//                    wordList = jsonData;
+                    wordList = shuffleArray(jsonData);
                 } else if (extension === 'json') {
                     // Handle JSON file
-                    wordList = JSON.parse(e.target.result);
+                    //wordList = JSON.parse(e.target.result);
+                    wordList = shuffleArray(JSON.parse(e.target.result));
                 } else {
                     console.error('Unsupported file type');
                     return;
                 }
 
                 currentIndex = 0;
-                displayWords();
+                currentSetIndex = 0
+                displayWords(wordList.slice(0, wordCountsInPage));
             } catch (error) {
                 console.error('Error processing file:', error);
             }
@@ -146,9 +157,11 @@ function handleFile(event) {
             reader.onload = function (e) {
                 try {
                     const data = JSON.parse(e.target.result);
-                    wordList = data.Sheet1;
-                    displayWords();
-                } catch (error) {
+//                    wordList = data.Sheet1;
+                    wordList = shuffleArray(data.Sheet1);
+                    currentSetIndex = 0
+                    displayWords(wordList.slice(0, wordCountsInPage)); 
+                    } catch (error) {
                     console.error('Error parsing JSON:', error);
                 }
             };
@@ -181,3 +194,27 @@ function showTranslation(translation) {
 
 }
 */
+
+
+function showNextWords() {
+    // Increase the current set index
+    currentSetIndex++;
+
+    // Calculate the start and end indices for the next set of words
+    const startIndex = currentSetIndex * wordCountsInPage;
+    const endIndex = (currentSetIndex + 1) * wordCountsInPage;
+
+
+    // Check if there are still words to display
+    if (endIndex-wordCountsInPage <= wordList.length) {
+        const nextSet = wordList.slice(startIndex, endIndex);
+        displayWords(nextSet);
+    } else {
+        console.log(startIndex);
+        console.log(endIndex);
+        console.log(wordList.length);
+    
+        // If no more words, you can handle it as you wish (e.g., start over or display a message)
+        console.log('No more words to display.');
+    }
+}
